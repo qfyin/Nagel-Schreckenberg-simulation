a725b3de5beddec3697e31a7fce497e320c3ef15
Qiufeng Yin <qfyin@microsoft.com>
2021-02-07 15:41:52 +0800

    data update

--- config/someLights.py
+++ config/someLights.py
@@ -14,13 +14,16 @@ length = 330 # 1600m, 5m/block
 maxSpeed = 5
 maxLength = 10000
 
-trafficLight1 = SpeedLimit(range=((100,0), (100,2)), limit=0, ticks=60, active=False)
+# Tsinghua West Entrance: 75 seconds Green, then 115 Red
+# A weekend noon: 20 ±5 cars in a round (190 second)
+
+trafficLight1 = SpeedLimit(range=((100,0), (100,2)), limit=0, ticks=90 * 1000 / updateFrame, active=False)
 QHFZ1 = SpeedLimit(range=((230,1), (245,1)), limit=2, ticks=0)
 QHFZ2 = SpeedLimit(range=((235,2), (245,2)), limit=1, ticks=0)
-trafficLight2 = SpeedLimit(range=((320,0), (320,2)), limit=0, ticks=60, active=False)
+trafficLight2 = SpeedLimit(range=((320,0), (320,2)), limit=0, ticks=90 * 1000 / updateFrame, active=False)
 
 speedLimits = [ trafficLight1, QHFZ1, QHFZ2, trafficLight2 ]
 # trafficGenerator = SimpleTrafficGenerator(1)
-carPerMinute = 30
+carPerMinute = 20
 trafficGenerator = PoissonTrafficGenerator(carPerMinute * updateFrame / (60 * 1000))
 slowDownProbability, laneChangeProbability = 0.4, 0.8
